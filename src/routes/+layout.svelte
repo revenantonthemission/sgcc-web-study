b1c07de0a0d3cb1f9f37177169b3905a186d4343
revenantonthemission <corpseonthemission@icloud.com>
2025-05-08 16:00:02 +0900

    /test 페이지에 간단한 버튼 구현

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 <nav>
 	<a href="/">home</a>
 	<a href="/about">about</a>
+	<a href="/test">test</a>
 </nav>
 
 {@render children()}
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  let count = $state(0);
+  
+  function increment() {
+    count += 1;
+  }
+  function decrement() {
+    count -= 1;
+  }
+  function reset() {
+    count = 0;
+  }
+</script>
+
+<button onclick={increment}>
+  Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
